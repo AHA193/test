bec9e762ab5f0b4ad213a0e6209efd3a4ba4d8da
Aziz <azizhoumani@gmail.com>
2025-02-21 16:28:52 +0100

    useState Hook 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  let color = "Blue";
+  return (
+  <>
+  <h1>My Favourite color is {color}</h1>
+    <button>Blue</button>
+  </>
+  );
+}
+
+export default App;

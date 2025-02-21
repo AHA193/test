d759d45e9b98df79b12e2f191e6ebf9c6b5e096f
Aziz <azizhoumani@gmail.com>
2025-02-21 18:19:07 +0100

    ex 3 updating a state based on previous state

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,23 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+// ex 3 _updating a state based on previous state
+
+const [count, setCount] = useState(0); 
 
 
-
-const [car, setCar] = useState({ 
-brand : "Ferrari",
-model: "Roma",
-year: "2023",
-color: "red"
-});
-
-const changeColor = ()=> {
-  setCar((prev)=>{   
-    return {...prev, color:"blue"} 
-  })
+const increaseCount = ()=>{
+setCount(count=>count + 1) 
+setCount(count=>count + 1)   
+setCount(count=>count + 1)  
+setCount(count=>count + 1)    
+            
 }
 
-  return (
+return (
   <>
-<h1>My {car.brand}</h1>
-<h2>It is a {car.color} {car.model} from {car.year}</h2>
-<button onClick={changeColor}>Blue</button>
+<h1>Count: {count}</h1>
+<button onClick={increaseCount}>Increase by 4</button>  
 
   </>
   );

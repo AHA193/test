8b66c79ea5799ca29572ad75167dfe553360c6b3
Aziz <azizhoumani@gmail.com>
2025-03-12 17:26:25 +0100

    useReducer Hook 7 - v1 Un compteur increment Decrement avec leUseState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import Profile from './components/Profile'
-import Footer from './components/Footer'
-//  to use the use context hook we have to create the context first
+
+// one simple counter that displays the count in this H1 then we have two
+// buttons and we have created this counter State using the use State Hook and the
+// initial value is zero and here we have added Setter function set count that
+// will increase the value by one and this decrease button will decrease the value
+// by one let me show you the web page also you can see here we have this
+// counter if I click on this increase button the counter value is
+// increasing and if I click on decrease button it is decreasing the counter
+// value now we will create the same counter functionality using use reducer
+// hook so first we have to import the useReducer from React
 
 
 function App() {  
 
-
+  const [count, setCount] = useState(0);
 
   return (    
 <>
-<Profile/>
-<Footer/>
+<h1>{count}</h1>
+<button onClick={()=>setCount(prev=>prev+1)}>Increase</button>
+<button onClick={()=>setCount(prev=>prev-1)}>Decrease</button>
 </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ContextProvider from './context/AppContext.jsx';
+
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ContextProvider>
+
       <App />
-  </ContextProvider>
+
 );
 
 

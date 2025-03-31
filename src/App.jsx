78bae586faaf55a67b7a679028a93a8c79989550
Aziz <azizhoumani@gmail.com>
2025-03-31 17:43:14 +0200

    useLayout Effect Hook 8 v1 executed before the useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import './App.css'
-import { useReducer } from 'react';
-
-// Now we will create the same counter functionality using use reducer
-// hook so first we have to import the useReducer from React
-
-// this use reducer accept two arguments the first one is reducer 
-// function and the second one is default state
-// useReducer(reducer, state)
-// useReducer(reducer, {count: 0})
-
 
 function App() {  
 
-  const initialState = {count: 0}
+useEffect(()=>{
+  console.log('Message from useEffect');
+},[])
 
-  const reducer  = (state, action) =>{
-    switch(action.type) {
-      case `increase` : {
-        return {count: state.count + 1}
-      }
-      case `decrease` : {
-        return {count: state.count - 1}
-      }
-      case `input` : {
-        return {count: action.payload}
-      }
-      default : {
-        return state
-      }
-    }
-
-  }
-  const [state, dispatch] = useReducer(reducer, initialState)
-
+useLayoutEffect(()=>{
+  console.log('Message from useLayoutEffect');
+},[])
 
   return (    
 <>
-<h1>{state.count}</h1>
-<button onClick={()=>dispatch({type: `increase`})}>Increase</button>
-<button onClick={()=>dispatch({type: `decrease`})}>Decrease</button>
-<br/>
-<input value={state.count}
-onChange={(e)=>dispatch({type: `input`,payload:Number(e.target.
-value)})}
-type="number" />
+<h2>TestMessage</h2>
 </>
-  )
+  );
 }
 
 export default App

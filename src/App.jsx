e3f50f53dce4039174c012e740bc219dad0fa445
Aziz <azizhoumani@gmail.com>
2025-03-31 20:27:10 +0200

    Plus one more state dans un composant useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {  
 
-  const [name, setName] = useState(
-    localStorage.getItem('username') ?
-    localStorage.getItem('username') : ''
-  );
-
-useEffect(()=>{
-  localStorage.setItem('username', name)
-}, [name])
+const [name, setName] = useLocalStorage('username', '')
+const [id, setId] = useLocalStorage('userid', '')
 
   return (    
 <>
 <input type="text" placeholder="enter your name"
 value={name} onChange={(e) => setName(e.target.value)} />
 <h2>Hello, {name}</h2>
+<input type="text" placeholder="enter Id"
+value={id} onChange={(e) => setId(e.target.value)} />
+<h2>Your id: {id}</h2>
 </>
   );
 }
